da2e97a10f8db79ba4195ec26211e74021048f55
ZICKOiQ <zicko.iq@gmail.com>
2025-05-27 23:15:25 +0300

    loger ip

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,12 +1,12 @@
 import 'package:http/http.dart' as http;
 
 class AppConfig {
-  static const String privateIp = "192.168.0.102";
-  static const String publicIp = "65.20.191.118";
+  static const String mainIP = "65.20.191.118";
+  static const String backupIP = "65.20.230.112";
   static const int pbPort = 8090;
   static const Duration _timeout = Duration(seconds: 2);
   
-  static String _currentIP = privateIp;
+  static String _currentIP = mainIP;
   static bool? _isLocalNetwork;
   
   static String get currentIP => _currentIP;
@@ -20,13 +20,13 @@ class AppConfig {
   }
 
   static bool isValidHost(String host) {
-    return host == privateIp || host == publicIp;
+    return host == mainIP || host == backupIP;
   }
 
   static Future<bool> checkLocalNetworkAccess() async {
     try {
       final response = await http
-          .get(Uri.parse('http://$privateIp:$pbPort/api/health'))
+          .get(Uri.parse('http://$mainIP:$pbPort/api/health'))
           .timeout(_timeout);
       return response.statusCode == 200;
     } catch (e) {
@@ -36,6 +36,6 @@ class AppConfig {
 
   static Future<void> initializeNetwork() async {
     _isLocalNetwork = await checkLocalNetworkAccess();
-    _currentIP = _isLocalNetwork! ? privateIp : publicIp;
+    _currentIP = _isLocalNetwork! ? mainIP : backupIP;
   }
 }
